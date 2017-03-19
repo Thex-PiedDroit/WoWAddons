a39eb1e2442562d2a7a32cf818b85c95a90da569
Thex <thex-pieddroit@hotmail.com>
2017-03-19 22:58:41 +0100

    Added previous version settings recovery
    /Temporary version, recovery will be removed at 1.3 release/

--- KillThemAll/Options.lua
+++ KillThemAll/Options.lua
@@ -1,7 +1,13 @@
 
 g_ktaOptions = {};
 
+local PreviousVersionRecovered = nil;
+
 function LoadOptions()
+
+	if PreviousVersionRecovered() then
+		return;
+	end
 
 	local defaultSoundChannel = (S_ktaOptions ~= nil and S_ktaOptions.default ~= nil and S_ktaOptions.default.soundChannel) or "Dialog";
 
@@ -20,4 +26,34 @@ function LoadOptions()
 
 	SetGods(GetWords((S_ktaOptions ~= nil and S_ktaOptions.gods) or g_ktaOptions.default.gods), true);
 	SetDelay((S_ktaOptions ~= nil and S_ktaOptions.minDelay) or g_ktaOptions.default.minDelay, (S_ktaOptions ~= nil and S_ktaOptions.maxDelay) or g_ktaOptions.default.maxDelay, true);
+end
+
+PreviousVersionRecovered = function()
+
+	local currentAddonVersion = GetAddOnMetadata("KillThemAll", "Version");
+
+	if S_AddonVersion == nil then
+		local defaultSoundChannel = "Dialog";
+
+		g_ktaOptions =
+		{
+			default =
+			{
+				gods = DefaultGods or "YSHAARJ",
+				soundChannel = defaultSoundChannel,
+				minDelay = DefaultMinDelay or 300,
+				maxDelay = DefaultMaxDelay or 1200,
+			},
+
+			soundChannel = SavedSoundChannel or defaultSoundChannel,
+		}
+
+		SetGods(GetWords(SavedGods or g_ktaOptions.default.gods), true);
+		SetDelay(SavedMinDelay or g_ktaOptions.default.minDelay, SavedMaxDelay or g_ktaOptions.default.maxDelay, true);
+
+		return true;
+	end
+
+	S_AddonVersion = currentAddonVersion;
+	return false;
 end
