db991453f460acc92134b5e68dff9909080dcc0e
Thex <thex-pieddroit@hotmail.com>
2017-03-22 19:31:22 +0100

    Added gods settings list binding on KTA_LichKing

--- KillThemAll_LichKing/Core.lua
+++ KillThemAll_LichKing/Core.lua
@@ -11,6 +11,12 @@ function events:ADDON_LOADED(arg)
 	if S_ktaOptions ~= nil and S_ktaOptions.gods ~= nil then
 		SetGods(GetWords(S_ktaOptions.gods), true);
 	end
+
+	if g_godsListSettings ~= nil then
+		AddGodToSettingsList(lichking);
+	else
+		KTA_Print("An update is available for KillThemAll");
+	end
 end
 
 eventsListener:SetScript("OnEvent", function(self, event, ...)
