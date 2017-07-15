a43f73148ded6b1b4d770ff1679a2ff203d2f655
Thex <thex-pieddroit@hotmail.com>
2017-07-15 14:18:25 +0200

    TEMP

--- Tongues/comm.lua
+++ Tongues/comm.lua
@@ -7,111 +7,100 @@ local tserial = LibStub("AceSerializer-3.0");
 	Tonguesc:RegisterComm("Tongues2", function(...) Tonguesc:TonguesRecieveRawMessage(...) end);
 
 
-function Tonguesc:TonguesSendMessage(channel, player, ...)
-	Tonguesc:SendCommMessage("Tongues2", tserial:Serialize({...}), channel, player);
+function Tonguesc:TonguesSendMessage(channel, player, messageDataPack)
+	Tonguesc:SendCommMessage("Tongues2", tserial:Serialize(messageDataPack), channel, player);
 end
 
-function Tonguesc:TonguesSendPrioritizedMessage(prio, channel, player, ...)
-	Tonguesc:SendCommMessage("Tongues2", tserial:Serialize({...}), channel, player, prio);
+function Tonguesc:TonguesSendPrioritizedMessage(prio, channel, player, messageDataPack)
+	Tonguesc:SendCommMessage("Tongues2", tserial:Serialize(messageDataPack), channel, player, prio);
 end
 
 function Tonguesc:TonguesRecieveRawMessage(cprefix, text, distribution, sender)
 	local sucess, t = tserial:Deserialize(text);
-	--print("COMM CHECK");
-	Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, unpack(t));
+	Tonguesc:TonguesReceiveMessage(cprefix, distribution, sender, t);
 end
 
 
 
 
 Tongues_ShowComm = false;
-function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ...)
+function Tonguesc:TonguesReceiveMessage(cprefix, distribution, sender, messageDataPack)
 
 	local TP = "<Tongues>";
 	local TPLen = string.len(TP);
 
 	--meta = string.match(meta, "^<Tongues>([%a%A]+)");
 	--print(meta);
+	local trans = messageDataPack.trans;
+	local language = messageDataPack.language or "";
+	local fluency = tonumber(messageDataPack.fluency) or 0;
+	local frame = messageDataPack.frame or 0;
+	local speakType = messageDataPack.speakType or "";
+	local channel = messageDataPack.channel or "";
+	local companionName = messageDataPack.companionName or "";
+	local message = messageDataPack.message or "";
+
+	local newMessageDataPack = {};
 
 	--REQUEST TRANSLATION------------------------------------------------------------------
 	if (trans == "RT") then
 
+		local language = string.match(language, "([%a%A]+):?");
+
 		--REMOVE TAG--
 		--pre = string.match(pre, "^:RT(:[%a%A]+)");
 		--print("after tag " ..pre);
-		local flu, chan, fram, lang = ...;
-		--POP FLUENCY
-		local fluency = 0;
-		fluency = flu;
-		fluency = tonumber(fluency);
-
-		--POP CHANNEL
-		local channel = "";
-		channel = chan;
-		--POP FRAME
-		local frame = 0;
-		frame = fram;
-
-		--POP LANGUAGE (looks for optional new : at the end)
-		local language = "";
-		language = string.match(lang, "([%a%A]+):?");
-
-		if language == nil then
-			language = "Upgrade";
-		end;
 
 		--local prepstring = "<Tongues>:TR:channel=" .. channel .. ":frame=" .. frame .. ":" .. "[" .. language .. "] " .. Tongues.PreviousSentMsg
 		--prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN)
 		--SendAddonMessage("Tongues", prepstring, "WHISPER", arg4)
-		Tonguesc:TonguesSendMessage("WHISPER", sender, "TR", channel, frame, language, Tongues.PreviousSentMsg);
+		newMessageDataPack.trans = "TR";
+		newMessageDataPack.channel = channel;
+		newMessageDataPack.frame = frame;
+		newMessageDataPack.language = language;
+		newMessageDataPack.message = Tongues.PreviousSentMsg;
+		print("1");
+		Tonguesc:TonguesSendMessage("WHISPER", sender, newMessageDataPack);
 
 	--TRANSLATION RESPONSE RECEIVED--------------------------------------------------------
 	elseif (trans == "TR") then
 		--REMOVE TAG--
 		--trans = string.match(meta, "^:TR(:[%a%A]+)");
-		--print(trans);
-		local chan, fram, lang, msgg = ...;
 
-		--POP CHANNEL
-		local channel = chan or "";
-
-		local frame = 0;
-		frame = fram;
-
-		local colortable = {};
+		local colorTable = {};
 		local prefix = "";
 		local postfix = "";
 
 		if channel == "CHAT_MSG_SAY" then
 			postfix = " says";
-			colortable = Tongues.Colors.SAY;
+			colorTable = Tongues.Colors.SAY;
 		elseif channel == "CHAT_MSG_YELL" then
 			postfix = " yells";
-			colortable = Tongues.Colors.YELL;
+			colorTable = Tongues.Colors.YELL;
 		elseif channel == "CHAT_MSG_PARTY" then
 			prefix = "[Party] ";
-			colortable = Tongues.Colors.PARTY;
+			colorTable = Tongues.Colors.PARTY;
 		elseif channel == "CHAT_MSG_GUILD" then
 			prefix = "[Guild] ";
-			colortable = Tongues.Colors.GUILD;
+			colorTable = Tongues.Colors.GUILD;
 		elseif channel == "CHAT_MSG_OFFICER" then
 			prefix = "[Officer] ";
-			colortable = Tongues.Colors.OFFICER;
+			colorTable = Tongues.Colors.OFFICER;
 		elseif channel == "CHAT_MSG_RAID" then
 			prefix = "[Raid] ";
-			colortable = Tongues.Colors.RAID;
+			colorTable = Tongues.Colors.RAID;
 		elseif channel == "CHAT_MSG_RAID_WARNING" then
-			colortable = Tongues.Colors.RAID_WARNING;
+			colorTable = Tongues.Colors.RAID_WARNING;
 		elseif channel == "CHAT_MSG_BATTLEGROUND" then
 			prefix = "[Battleground] ";
-			colortable = Tongues.Colors.BATTLEGROUND;
+			colorTable = Tongues.Colors.BATTLEGROUND;
 		elseif channel == "CHAT_MSG_PARTY_LEADER" then
 			prefix = "[Party Leader]";
-			colortable = Tongues.Colors.PARTY_LEADER;
+			colorTable = Tongues.Colors.PARTY_LEADER;
 		end
 
 		sender = Tongues.GetCharacterWho(sender);
-		local prepstring = prefix .. sender .. postfix .. ": " .. "[" ..lang.. "] " .. msgg;
+		local prepstring = prefix .. sender .. postfix .. ": " .. "[" .. language .. "] " .. message;
 		prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
 
 
@@ -119,10 +108,10 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 
 			if UnitClass("player") == "Mage" and UnitBuff("player", "Arcane Language") and TonguesmageKnownLang == true then
 				--We are using Arcane languages, do NOTHING, ABSOLUTLY NOTHING
-				prepstring = prefix ..sender..postfix .. ": " .. msgg;
+				prepstring = prefix .. sender .. postfix .. ": " .. message;
 				prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
 			else
-				_G["ChatFrame" .. frame]:AddMessage(prepstring, colortable[1], colortable[2], colortable[3]);
+				_G["ChatFrame" .. frame]:AddMessage(prepstring, colorTable[1], colorTable[2], colorTable[3]);
 			end
 		end
 
@@ -130,41 +119,30 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 	elseif (trans == "RV") then
 		--local prepstring = "<Tongues>:VR:version=" .. Tongues.Version .. ":";
 		--prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
-		Tonguesc:TonguesSendMessage("WHISPER", sender, "VR", Tongues.Version);
+		newMessageDataPack.trans = "VR";
+		newMessageDataPack.version = Tongues.Version;
+		print("2");
+		Tonguesc:TonguesSendMessage("WHISPER", sender, newMessageDataPack);
 
 	-- VERSION RECEIVED
 	elseif (trans == "VR") then
 		--REMOVE TAG--
 		--arg2 = string.match(arg2, "^:VR(:[%a%A]+)");
-		local vers = ...;
-		--POP VERSION
-		local version = "";
-		version = vers;
+		local version = messageDataPack.version or "";
 
 		SELECTED_CHAT_FRAME:AddMessage("(My Tongues version is v" .. version .. ")", 1, 1, 0);
 
 	-- REQUEST LEARN
 	elseif (trans == "RL") then
-		--REMOVE TAG--
-		local lang, fact, rac, clas, fram, flu = ...;
-
-		--POP LANGUAGE
-		local language = lang or "";
 
 		--POP FACTION
-		local faction = fact or "";
+		local faction = messageDataPack.faction or "";
 
 		--POP RACE
-		local race = rac or "";
+		local race = messageDataPack.race or "";
 
 		--POP CLASS
-		local class = clas or "";
-
-		--POP FRAME
-		local frame = fram or 0;
-
-		--POP FLUENCY
-		local fluency = tonumber(flu or "0");
+		local class = messageDataPack.class or "";
 
 		local learn = 1;
 		if  (Tongues.Language[language] ~= nil and Tongues.Language[language].Difficulty ~= nil)
@@ -177,13 +155,18 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 			local c = Tongues.Language[language].Difficulty[class] or 0;
 			local result = d + f + r + c;
 
+			newMessageDataPack.trans = "LR";
+			newMessageDataPack.language = language;
+			newMessageDataPack.learn = learn;
+
 			--local seed = math.random(0,2147483647)+(GetTime()*1000);
 			--if result <= 0 then result = 100 end
 			local randomres = math.random(1, (result + 1));
 			if math.random(1, (result + 1)) >= randomres and not (TTimerFrame:IsShown()) then
 
 				if not TTimerFrame:IsShown() then
-					Tonguesc:TonguesSendMessage("WHISPER", sender, "LR", language, learn);
+					print("3");
+					Tonguesc:TonguesSendMessage("WHISPER", sender, newMessageDataPack);
 					TTimerFrame:Show();
 				end
 			else
@@ -195,7 +178,8 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 				if math.random(1, result+1) >= randomres and (math.random(1, 100) > 50) and not TTimerFrame:IsShown() then
 
 					if not TTimerFrame:IsShown() then
-						Tonguesc:TonguesSendMessage("WHISPER", sender, "LR", language, learn);
+						print("4");
+						Tonguesc:TonguesSendMessage("WHISPER", sender, newMessageDataPack);
 						TTimerFrame:Show();
 					end
 				end
@@ -206,13 +190,8 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 	elseif (trans == "LR") then
 		--REMOVE TAG--
 
-		local lang, lrn = ...;
-
-		--POP LANGUAGE
-		local language = lang or "";
-
 		--POP LEARN
-		local learn = tonumber(lrn or "0");
+		local learn = tonumber(messageDataPack.learn or "0");
 
 		if Tongues.Settings.Character.Fluency[language] == nil then
 			Tongues.Settings.Character.Fluency[language] = 0;
@@ -231,57 +210,32 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 
 	-- REQUEST PETSPEAK
 	elseif (trans == "PR") then
-		--REMOVE TAG--
-		local flu, petn, fram, lang, pst = ...;
 
-		--POP FLUENCY
-		local fluency = tonumber(flu or 0);
-
-		--POP PETNAME
-		local petname = petn or "";
-
-		--POP FRAME
-		local frame = fram or 0;
-
-		--POP LANGUAGE (looks for optional new : at the end)
-		local language = lang or "";
-		--POP PETSPEAK
-		local petspeaktype = pst or "";
-
-		local prepstring = "<Tongues>:RP:fluency=" .. fluency .. ":frame=" .. frame .. ":language=" .. language .. ":petname=" .. UnitName("pet") .. ":petspeaktype=" .. petspeaktype .. ":" .. Tongues.PreviousPetSentMsg;
+		local prepstring = "<Tongues>:RP:fluency=" .. fluency .. ":frame=" .. frame .. ":language=" .. language .. ":petname=" .. UnitName("pet") .. ":petspeaktype=" .. companionSpeakType .. ":" .. Tongues.PreviousPetSentMsg;
 		prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
-		Tonguesc:TonguesSendMessage("WHISPER", sender, "RP", fluency, frame, language, UnitName("pet"), petspeaktype, Tongues.PreviousPetSentMsg);
+
+		newMessageDataPack.trans = "RP";
+		newMessageDataPack.fluency = fluency;
+		newMessageDataPack.frame = frame;
+		newMessageDataPack.language = language;
+		newMessageDataPack.companionName = UnitName("pet");
+		newMessageDataPack.companionSpeakType = companionSpeakType;
+		newMessageDataPack.message = Tongues.PreviousPetSentMsg;
+		print("5");
+		Tonguesc:TonguesSendMessage("WHISPER", sender, newMessageDataPack);
 
 
 	-- PETSPEAK RECEIVED
 	elseif (trans == "RP") then
-		--REMOVE TAG--
-		local flu, fram, lang, petn, pst, pmsg = ...;
 
-		--POP FLUENCY
-		local fluency = tonumber(flu or "0");
-
-		--POP FRAME
-		local frame = fram or 0;
-
-		--POP LANGUAGE (looks for optional new : at the end)
-		local language = lang or "";
-
-		--POP PETNAME
-		local petname = petn or "";
-
-		--POP PETNAME
-		local petspeaktype = pst or "";
-
-		local petspeak = pmsg;
 		if petspeak ~= nil then
 			local prepstring = ""
 			if  (language == "Common" and UnitFactionGroup("player") == "Alliance")
 				or (language == "Orcish" and UnitFactionGroup("player") == "Horde")
 				or (language == nil) then
-				prepstring = "[" .. petname .. "] " .. petspeaktype .. ": " .. petspeak;
+				prepstring = "[" .. petname .. "] " .. companionSpeakType .. ": " .. message;
 			else
-				prepstring = "[" .. petname .. "] " .. petspeaktype .. ": " .. "[" .. language .. "] " .. petspeak;
+				prepstring = "[" .. petname .. "] " .. companionSpeakType .. ": " .. "[" .. language .. "] " .. message;
 			end;
 
 			prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
@@ -290,79 +244,41 @@ function Tonguesc:TonguesReceiveMessage(cprefix, sender, distribution, trans, ..
 
 	----MOUNT SPEAK TRANSLATION
 	elseif (trans == "RMN") then
-		--REMOVE TAG--
-		local flu, mountn, fram, lang, pst = ...;
 
-		--POP FLUENCY
-		local fluency = 0;
-		fluency = flu;
-		fluency = tonumber(fluency);
-
-		--POP PETNAME
-		local mountname = "";
-		mountname = mountn;
-
-		--POP FRAME
-		local frame = 0;
-		frame = fram;
-
-		--POP LANGUAGE (looks for optional new : at the end)
-		local language = "";
-		language = lang;
-		--POP PETSPEAK
-		local mountspeaktype = "";
-		mountspeaktype = pst;
-
-		local prepstring = "<Tongues>:RP:fluency=" .. fluency .. ":frame=" .. frame .. ":language=" .. language .. ":mountname=" .. mountname .. ":mountspeaktype=" .. mountspeaktype .. ":" .. Tongues.PreviousMountSentMsg;
+		local prepstring = "<Tongues>:RP:fluency=" .. fluency .. ":frame=" .. frame .. ":language=" .. language .. ":mountname=" .. companionName .. ":mountspeaktype=" .. companionSpeakType .. ":" .. Tongues.PreviousMountSentMsg;
 		prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
-		Tonguesc:TonguesSendMessage("WHISPER", sender, "MNR", fluency, frame, language, mountname, mountspeaktype, Tongues.PreviousMountSentMsg);
+		newMessageDataPack.trans = "MNR";
+		newMessageDataPack.fluency = fluency;
+		newMessageDataPack.frame = frame;
+		newMessageDataPack.language = language;
+		newMessageDataPack.companionName = companionName;
+		newMessageDataPack.companionSpeakType = companionSpeakType;
+		newMessageDataPack.message = Tongues.PreviousMountSentMsg;
+		print("6");
+		Tonguesc:TonguesSendMessage("WHISPER", sender, "MNR", fluency, frame, language, companionName, companionSpeakType, Tongues.PreviousMountSentMsg);
 
 	-- MOUNTSPEAK RECEIVED
 	elseif (trans == "MNR") then
-		--REMOVE TAG--
-		local flu, fram, lang, mountn, pst, pmsg = ...;
 
-		--POP FLUENCY
-		local fluency = 0;
-		fluency = flu;
-		fluency = tonumber(fluency);
-
-		--POP FRAME
-		local frame = 0;
-		frame = fram;
-
-		--POP LANGUAGE (looks for optional new : at the end)
-		local language = "";
-		language = lang;
-
-		--POP PETNAME
-		local mountname = "";
-		mountname = mountn;
-
-		--POP PETNAME
-		local mountspeaktype = "";
-		mountspeaktype = pst;
-
-		local mountspeak = pmsg;
-		if mountspeak ~= nil then
+		if message ~= nil then
 			local prepstring = ""
 			if  (language == "Common" and UnitFactionGroup("player") == "Alliance")
 				or (language == "Orcish" and UnitFactionGroup("player") == "Horde")
 				or (language == nil) then
 
-				prepstring = "[" ..UnitName("player")"'s " .. mountname .. "] " .. mountspeaktype .. ": " .. mountspeak;
+				prepstring = "[" ..UnitName("player")"'s " .. companionName .. "] " .. companionSpeakType .. ": " .. message;
 
 			elseif sender == UnitName("player") and ((language == "Common" and UnitFactionGroup("player") == "Alliance")
 				or (language == "Orcish" and UnitFactionGroup("player") == "Horde")
 				or (language == nil)) then
 
-				prepstring = "[" ..mountname .. "] " .. mountspeaktype .. ": " .. mountspeak;
+				prepstring = "[" .. companionName .. "] " .. companionSpeakType .. ": " .. message;
 
 			elseif sender == UnitName("player") then
-				prepstring = "[" .. mountname .. "] " .. mountspeaktype .. ": " .. "[" .. language .. "] " .. mountspeak;
+				prepstring = "[" .. companionName .. "] " .. companionSpeakType .. ": " .. "[" .. language .. "] " .. message;
 
 			else
-				prepstring = "[" .. UnitName("player")"'s " .. mountname .. "] " .. mountspeaktype .. ": " .. "[" .. language .. "] " .. mountspeak;
+				prepstring = "[" .. UnitName("player")"'s " .. companionName .. "] " .. companionSpeakType .. ": " .. "[" .. language .. "] " .. message;
 			end;
 
 			prepstring = string.sub(prepstring, 1, TONGUES_MAX_MSG_LEN);
