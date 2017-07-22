d4ccebfc61cd2672d0f7be7af1a89ccb719653af
Thex <thex-pieddroit@hotmail.com>
2017-07-22 19:43:45 +0200

    Added main inner-frame + basic options
    - Dropdown list of poll types
    - "Allow new answers"
    - "Allow multiple votes"

--- PollCraft/CreatePollFrame.lua
+++ PollCraft/CreatePollFrame.lua
@@ -6,6 +6,9 @@ local mainFrameSize =
 	x = 800,
 	y = 600
 };
+local framesMargin = (mainFrameSize.x / 40);
+
+local CreatePollTypesDropdownList = nil;
 
 
 function InitCreatePollFrame()
@@ -19,5 +22,54 @@ function InitCreatePollFrame()
 	mainFrameTitle:SetPoint("CENTER", 0, 0);
 
 
+
+	local optionsFrameSize =
+	{
+		x = mainFrameSize.x - framesMargin,
+		y = mainFrameSize.y - (framesMargin * 4)
+	};
+
+	local newPollFrame = CreateBackdroppedFrame("CreatePollFrame", mainFrame, optionsFrameSize.x, optionsFrameSize.y);
+	newPollFrame:SetPoint("BOTTOM", 0, mainFrameSize.x / 80);
+
+	local pollTypesListPosY = -50;
+	CreatePollTypesDropdownList(newPollFrame, pollTypesListPosY);
+
+	local allowNewAnswersLabel = CreateLabel(newPollFrame, "Allow users to add answers", 16);
+	allowNewAnswersLabel:SetPoint("TOPLEFT", framesMargin + 220, pollTypesListPosY + 22);
+	local allowNewAnswersCheck = CreateCheckButton(newPollFrame, "AllowNewAnswersCheckButton");
+	allowNewAnswersCheck:SetPoint("TOPLEFT", framesMargin + 195, pollTypesListPosY + 25);
+
+	local allowMultipleVotesLabel = CreateLabel(newPollFrame, "Allow multiple votes", 16);
+	allowMultipleVotesLabel:SetPoint("TOPLEFT", framesMargin + 220, pollTypesListPosY - 5);
+	local allowMultipleVotesCheck = CreateCheckButton(newPollFrame, "AllowMultipleVotesCheckButton");
+	allowMultipleVotesCheck:SetPoint("TOPLEFT", framesMargin + 195, pollTypesListPosY - 2);
+
+
 	g_createPollFrame.panel = mainFrame;
+end
+
+
+CreatePollTypesDropdownList = function(parentFrame, posY)
+
+	local listLabel = CreateLabel(parentFrame, "Poll Type", 16);
+	listLabel:SetPoint("TOPLEFT", framesMargin + 3, posY + 22);
+
+	local availableOptions =
+	{
+		{
+			text = "Direct (raid/party)",
+			value = "RAID",
+			checked = true
+		},
+		{
+			text = "Permanent (guild)",
+			value = "GUILD"
+		},
+	}
+
+	local pollTypesList = CreateDropDownList("PollTypesList", parentFrame, 140, availableOptions);
+	pollTypesList:SetPoint("TOPLEFT", 0, posY);
+	UIDropDownMenu_SetSelectedValue(pollTypesList, availableOptions[1].value, false);
+	parentFrame.pollTypesDropDownList = pollTypesList;
 end
