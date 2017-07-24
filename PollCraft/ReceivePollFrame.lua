c0ca8c4f28404fa63e8fca9a2bc200d01697957b
Thex <thex-pieddroit@hotmail.com>
2017-07-24 18:41:47 +0200

    Added ability to load answers from received data

--- PollCraft/ReceivePollFrame.lua
+++ PollCraft/ReceivePollFrame.lua
@@ -8,6 +8,8 @@ local mainFrameSize =
 }
 local framesMargin = (mainFrameSize.x / 40);
 
+local answersParentFrame = nil;
+
 
 function InitializeReceivePollFrame()
 
@@ -62,10 +64,84 @@ function InitializeReceivePollFrame()
 	local answersFrame = CreateScrollFrame("AnswersFrame_InterfacePoll", newPollFrame, answersFrameSize.x, answersFrameSize.y);
 	answersFrame:SetPoint("TOP", 0, answersFramePosY);
 
+	answersFrame.content.answersBoxes = {};
+	answersParentFrame = answersFrame.content;
+
 
 	local sendVoteButton = CreateButton("SendVoteButton", newPollFrame, 120, 30, "Send vote");
 	sendVoteButton:SetPoint("TOP", 0, answersFramePosY - answersFrameSize.y - (framesMargin * 0.4));
 
 
 	g_receivePollFrame.panel = mainFrame;
+end
+
+
+local answersCount = 0;
+local answersObjects = {};
+local answersFramesHeight = 70;
+
+function LoadAnswer(answerText, trueCheckFalseTickButton)
+
+	local object = nil;
+
+	if answersCount < #answersObjects then
+		object = answersObjects[answersCount + 1];
+		object.text:SetText(answerText);
+		object.number:Show();
+		object.text:Show();
+	else
+		local answerIndexStr = tostring(answersCount + 1);
+
+		local answerFrameWidth = answersParentFrame:GetWidth() - (framesMargin * 4) - 40;
+		local textFramePosY = -10 - ((answersFramesHeight + (framesMargin * 0.25)) * answersCount);
+		local textFrame = CreateBackdroppedFrame("Answer" .. answerIndexStr .. "TextFrame", answersParentFrame, answerFrameWidth, answersFramesHeight);
+		textFrame:SetPoint("TOPLEFT", framesMargin + 24, textFramePosY);
+		local newText = CreateLabel(textFrame, answerText, 16, answerFrameWidth - 20, "LEFT");
+		newText:SetPoint("TOPLEFT", framesMargin, -11);
+
+		local newNumber = CreateLabel(answersParentFrame, answerIndexStr .. '.', 16);
+		newNumber:SetPoint("TOPLEFT", framesMargin - 5, textFramePosY - 8);
+
+		local newCheck = CreateCheckButton(answersParentFrame, "Answer" .. answerIndexStr .. "CheckButton");
+		newCheck:SetPoint("RIGHT", textFrame, "RIGHT", framesMargin + 10, 0);
+
+		local newTick = CreateRadioCheckButton(answersParentFrame, "Answer" .. answerIndexStr .. "CheckButton");
+		newTick:SetPoint("RIGHT", textFrame, "RIGHT", framesMargin + 8, 0);
+
+		object =
+		{
+			text = newText,
+			number = newNumber,
+			voteCheck = newCheck,
+			voteTick = newTick
+		}
+
+		table.insert(answersObjects, object);
+	end
+
+	if trueCheckFalseTickButton then
+		object.voteCheck:Show();
+		object.voteTick:Hide();
+	else
+		object.voteCheck:Hide();
+		object.voteTick:Show();
+	end
+
+	answersCount = answersCount + 1;
+end
+
+function MakeAllTicksExclusive()
+
+	for i = 1, #answersObjects do
+		local currentObjectTick = answersObjects[i].voteTick;
+		currentObjectTick.index = i;
+
+		currentObjectTick:SetScript("OnClick", function(self)
+			for j = 1, #answersObjects do
+				if j ~= currentObjectTick.index then
+					answersObjects[j].voteTick:SetChecked(false);
+				end
+			end
+		end);
+	end
 end
