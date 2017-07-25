016077c5fc9b47de9eaf9df9a5cc2beb1d47f81c
Thex <thex-pieddroit@hotmail.com>
2017-07-26 00:50:50 +0200

    Added ability to add additional answers to ongoing poll

--- PollCraft/ReceivePollFrame.lua
+++ PollCraft/ReceivePollFrame.lua
@@ -12,6 +12,8 @@ local answersParentFrame = nil;
 local answersScrollFrame = nil;
 local answersCount = 0;
 
+local HideAllAnswers = nil;
+
 
 function InitializeReceivePollFrame()
 
@@ -82,25 +84,47 @@ function InitializeReceivePollFrame()
 
 	mainFrame:Hide();
 	mainFrame:SetScript("OnShow", function() g_currentlyBusy = true; end);
-	mainFrame:SetScript("OnHide", function() g_currentlyBusy = false; answersCount = 0; end);
+	mainFrame:SetScript("OnHide", function() HideAllAnswers(); g_currentlyBusy = false; answersCount = 0; end);
 	g_receivePollFrame.panel = mainFrame;
 end
 
 
-local answersObjects = {};
+local AddOrRemoveAdditionalAnswerEditBox = nil;
+local RemoveAdditionalAnswer = nil;
+
+local answerObjects = {};
 local marginBetweenAnswers = 10;
 local answersFramesHeight = 70;
 local totalHeightOfEachAnswer = answersFramesHeight + (framesMargin * 0.25);
 
-function LoadAnswer(answerText, trueCheckFalseTickButton)
+local additionalAnswerEditBoxHeight = answersFramesHeight - 12;
+
+local allowAdditionalAnswers = false;
+local allowMultipleVotes = false;
+
+function LoadAnswer(answerText)
 
 	local object = nil;
 
-	if answersCount < #answersObjects then
-		object = answersObjects[answersCount + 1];
-		object.text:SetText(answerText);
+	local isAdditionalAnswer = answerText == nil or answerText == "";
+
+	if answersCount < #answerObjects then
+		object = answerObjects[answersCount + 1];
+
+		if isAdditionalAnswer then
+			object.text:Hide();
+			object.textFrame:Hide();
+			object.editBoxScrollFrame.EditBox:SetText("");
+			object.editBoxScrollFrame:Show();
+			object.deleteButton:Show();
+		else
+			object.text:SetText(answerText);
+			object.text:Show();
+			object.textFrame:Show();
+			object.editBoxScrollFrame:Hide();
+			object.deleteButton:Hide();
+		end
 		object.number:Show();
-		object.text:Show();
 	else
 		local answerIndexStr = tostring(answersCount + 1);
 
@@ -108,51 +132,168 @@ function LoadAnswer(answerText, trueCheckFalseTickButton)
 		local textFramePosY = -((totalHeightOfEachAnswer * answersCount) + 10);
 		local textFrame = CreateBackdroppedFrame("Answer" .. answerIndexStr .. "TextFrame", answersParentFrame, answerFrameWidth, answersFramesHeight);
 		textFrame:SetPoint("TOPLEFT", framesMargin + 24, textFramePosY);
-		local newText = CreateLabel(textFrame, answerText, 16, answerFrameWidth - 20, "LEFT");
-		newText:SetPoint("TOPLEFT", framesMargin, -11);
+		local answerLabel = CreateLabel(textFrame, answerText, 16, answerFrameWidth - 20, "LEFT");
+		answerLabel:SetPoint("TOPLEFT", framesMargin, -11);
+
+		local additionalAnswerEditBoxWidth = answerFrameWidth - 12;
+		local additionalAnswerEditBox = CreateEditBox("AdditionalAnswer" .. answerIndexStr .. "EditBox", answersParentFrame, additionalAnswerEditBoxWidth, additionalAnswerEditBoxHeight, false, AddOrRemoveAdditionalAnswerEditBox, answersCount + 1, 16);
+		additionalAnswerEditBox:SetPoint("TOPLEFT", framesMargin + 30, textFramePosY - 7);
+
+		local deleteButton = CreateIconButton("DeleteAdditionalAnswer" .. answerIndexStr .. "Button", answersParentFrame, 20, "Interface/Buttons/Ui-grouploot-pass-up", "Interface/Buttons/Ui-grouploot-pass-down", nil, RemoveAdditionalAnswer, answersCount + 1);
+		deleteButton:SetPoint("TOPLEFT", framesMargin + 28.5 + additionalAnswerEditBoxWidth + 12, textFramePosY -2);
+
+		if isAdditionalAnswer then
+			textFrame:Hide();
+			answerLabel:Hide();
+			if not isAdditionalAnswer then
+				deleteButton:Hide();
+			end
+		else
+			additionalAnswerEditBox:Hide();
+			deleteButton:Hide();
+		end
 
 		local newNumber = CreateLabel(answersParentFrame, answerIndexStr .. '.', 16);
 		newNumber:SetPoint("TOPLEFT", framesMargin - 5, textFramePosY - 8);
 
 		local newCheck = CreateCheckButton(answersParentFrame, "Answer" .. answerIndexStr .. "CheckButton");
 		newCheck:SetPoint("RIGHT", textFrame, "RIGHT", framesMargin + 10, 0);
+		newCheck:Hide();
 
 		local newTick = CreateRadioCheckButton(answersParentFrame, "Answer" .. answerIndexStr .. "CheckButton");
 		newTick:SetPoint("RIGHT", textFrame, "RIGHT", framesMargin + 8, 0);
+		newTick:Hide();
 
 		object =
 		{
-			text = newText,
+			textFrame = textFrame,
+			text = answerLabel,
+			editBoxScrollFrame = additionalAnswerEditBox,
+			deleteButton = deleteButton;
 			number = newNumber,
 			voteCheck = newCheck,
 			voteTick = newTick
 		}
 
-		table.insert(answersObjects, object);
+		table.insert(answerObjects, object);
+		MakeAllTicksExclusive()
 	end
 
-	if trueCheckFalseTickButton then
-		object.voteCheck:Show();
-		object.voteTick:Hide();
-	else
-		object.voteCheck:Hide();
-		object.voteTick:Show();
+	if not isAdditionalAnswer or not allowMultipleVotes then
+		if allowMultipleVotes then
+			object.voteCheck:Show();
+			object.voteCheck:SetChecked(false);
+			object.voteTick:Hide();
+		else
+			object.voteCheck:Hide();
+			object.voteTick:Show();
+			object.voteTick:SetChecked(false);
+		end
 	end
 
 	answersCount = answersCount + 1;
 	UpdateScrollBar(answersScrollFrame, answersCount * totalHeightOfEachAnswer);
 end
 
+AddOrRemoveAdditionalAnswerEditBox = function(currentlyModifiedAnswerIndex)
+
+	if not allowAdditionalAnswers or not allowMultipleVotes then
+		return;
+	end
+
+
+	local object = answerObjects[currentlyModifiedAnswerIndex];
+	local boxText = object.editBoxScrollFrame.EditBox:GetText();
+
+	if boxText == nil or boxText == "" then
+		RemoveAdditionalAnswer(currentlyModifiedAnswerIndex);
+
+	elseif currentlyModifiedAnswerIndex == answersCount then
+		if answersCount < #answerObjects then
+
+			local nextObject = answerObjects[currentlyModifiedAnswerIndex + 1];
+			nextObject.number:Show();
+			nextObject.editBoxScrollFrame.EditBox:SetText("");
+			nextObject.editBoxScrollFrame:Show();
+
+			if allowAdditionalAnswers then
+				nextObject.deleteButton:Show();
+			else
+				nextObject.deleteButton:Hide();
+			end
+			nextObject.textFrame:Hide();
+			nextObject.text:Hide();
+			answersCount = answersCount + 1;
+		else
+			LoadAnswer("");
+		end
+
+		object.voteCheck:Show();
+		object.voteCheck:SetChecked(false);
+	end
+
+	UpdateScrollBar(answersScrollFrame, answersCount * totalHeightOfEachAnswer);
+end
+
+RemoveAdditionalAnswer = function(index)
+
+	if index == answersCount then
+		local editBox = answerObjects[index].editBoxScrollFrame.EditBox;
+		editBox:SetText("");
+		editBox:ClearFocus();
+		editBox:HighlightText(0, 0);	-- Forces highlight removal
+		answerObjects[index].voteCheck:Hide();
+		return;
+	end
+
+	for i = index, answersCount - 1 do
+
+		local currentObject = answerObjects[i];
+		local nextObject = answerObjects[i + 1];
+		currentObject.editBoxScrollFrame.EditBox:SetText(nextObject.editBoxScrollFrame.EditBox:GetText());
+		currentObject.voteCheck:SetChecked(nextObject.voteCheck:GetChecked());
+
+		if i + 1 == answersCount then
+			nextObject.number:Hide();
+			nextObject.editBoxScrollFrame:Hide();
+			nextObject.deleteButton:Hide();
+			nextObject.voteCheck:Hide();
+
+			currentObject.voteCheck:Hide();
+		end
+	end
+
+	answersCount = answersCount - 1;
+	UpdateScrollBar(answersScrollFrame, answersCount * totalHeightOfEachAnswer);
+end
+
+local function HideAnswer(answerObject)
+	answerObject.textFrame:Hide();
+	answerObject.text:Hide();
+	answerObject.editBoxScrollFrame:Hide();
+	answerObject.deleteButton:Hide();
+	answerObject.number:Hide();
+	answerObject.voteCheck:Hide();
+	answerObject.voteTick:Hide();
+end
+
+HideAllAnswers = function()
+	for i = 1, #answerObjects do
+		HideAnswer(answerObjects[i]);
+	end
+end
+
+
 function MakeAllTicksExclusive()
 
-	for i = 1, #answersObjects do
-		local currentObjectTick = answersObjects[i].voteTick;
+	for i = 1, #answerObjects do
+		local currentObjectTick = answerObjects[i].voteTick;
 		currentObjectTick.index = i;
 
 		currentObjectTick:SetScript("OnClick", function(self)
-			for j = 1, #answersObjects do
+			for j = 1, #answerObjects do
 				if j ~= currentObjectTick.index then
-					answersObjects[j].voteTick:SetChecked(false);
+					answerObjects[j].voteTick:SetChecked(false);
 				end
 			end
 		end);
@@ -186,13 +327,14 @@ function LoadAndOpenReceivePollFrame(pollData, sender, senderRealm)
 
 		g_receivePollFrame.panel.questionLabel:SetText(pollData.question);
 
-		local multipleVotesAllowed = pollData.multiVotes;
+		allowMultipleVotes = pollData.multiVotes;
+		allowAdditionalAnswers = pollData.allowNewAnswers;
 
 		for i = 1, #pollData.answers do
-			LoadAnswer(pollData.answers[i], multipleVotesAllowed);
+			LoadAnswer(pollData.answers[i]);
 		end
-		if not multipleVotesAllowed then
-			MakeAllTicksExclusive();
+		if allowAdditionalAnswers then
+			LoadAnswer("");
 		end
 	end
 
