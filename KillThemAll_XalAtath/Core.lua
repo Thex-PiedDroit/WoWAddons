522943e0805d314c019651e6a0e12716128981f6
Thex <thex-pieddroit@hotmail.com>
2017-09-02 13:36:10 +0200

    Created Xal'Atath module for KTA

---
+++ KillThemAll_XalAtath/Core.lua
@@ -0,0 +1,35 @@
+
+g_cerberus.RegisterAddonModule("KillThemAll");
+
+local eventsListener = CreateFrame("Frame");
+local events = {};
+
+function events:ADDON_LOADED(arg)
+
+	if arg ~= "KillThemAll_XalAtath" then
+		return;
+	end
+
+	if g_godsListSettings ~= nil then
+		if S_ktaOptions ~= nil and S_ktaOptions.sGods ~= nil then
+
+			local currentGods = GetWords(string.upper(S_ktaOptions.sGods));
+			if TableContains(currentGods, g_xalAtath.sDataName) then
+				SetGods(currentGods, true);
+			end
+		end
+
+		AddGodToSettingsList(g_xalAtath);
+	else
+		KTA_Print("An update is available for KillThemAll. KillThemAll_XalAtath will not work until both are updated.");
+		return;
+	end
+end
+
+eventsListener:SetScript("OnEvent", function(self, event, ...)
+	events[event](self, ...);
+end);
+
+for k, v in pairs(events) do
+	eventsListener:RegisterEvent(k);
+end
