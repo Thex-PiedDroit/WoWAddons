74fb2b0ed7572b4f9ad749984b4d403f309ac386
Thex <thex-pieddroit@hotmail.com>
2019-07-11 16:33:22 -0400

    Added aluneth module from Laiev

---
+++ KillThemAll_Aluneth/Aluneth.lua
@@ -0,0 +1,28 @@
+
+Cerberus_HookThisFile();
+
+g_Aluneth = g_soundLibrary:Inherit({ m_sDisplayName = "Aluneth", m_sDataName = "Aluneth" });
+g_Aluneth.m_generalSoundFilesList =
+{
+	1396767,--"sound/creature/aluneth/vo_703_aluneth_06.ogg",	-- My essence will not be controlled again.
+	1396777,--"sound/creature/aluneth/vo_703_aluneth_16.ogg",	-- Such power, it course through me.
+	1396780,--"sound/creature/aluneth/vo_703_aluneth_19.ogg",	-- Arcane energy meets this world together, do you feel child?
+	1396785,--"sound/creature/aluneth/vo_703_aluneth_24.ogg",	-- Your allies are too dependents on you, let them starve.
+	1396788,--"sound/creature/aluneth/vo_703_aluneth_27.ogg",	-- In eons past, the Titans used the language of the Arcane to (...)
+	1396792,--"sound/creature/aluneth/vo_703_aluneth_31.ogg",	-- Even in intensive study, the mortal mages acquire so little knowledge in a lifetime. (...)
+	1396801,--"sound/creature/aluneth/vo_703_aluneth_40.ogg",	-- Queen Azshjara.. Do not underestimate her grasp of the Arcane (...)
+	1396823,--"sound/creature/aluneth/vo_703_aluneth_62.ogg",	-- The filth that occupies this place are an insult to the glory of (...)
+	1396826,--"sound/creature/aluneth/vo_703_aluneth_65.ogg",	-- Have you ever considered opening portals to other worlds? (...)
+	1396838,--"sound/creature/aluneth/vo_703_aluneth_77.ogg",	-- Power.. I must have more power!
+	1396839,--"sound/creature/aluneth/vo_703_aluneth_78.ogg",	-- Is that everything you have?
+	1396849,--"sound/creature/aluneth/vo_703_aluneth_88.ogg",	-- For generations, the mortal mages of Dalaran have sought to (...)
+};
+g_Aluneth.m_deathSoundFilesList =
+{
+	1396806,--"sound/creature/aluneth/vo_703_aluneth_45.ogg",	-- Are you afraid to fight?! Interesting.
+	1396816,--"sound/creature/aluneth/vo_703_aluneth_55.ogg",	-- Yes... Yes...
+	1396817,--"sound/creature/aluneth/vo_703_aluneth_56.ogg",	-- I'll consume it.
+	1396819,--"sound/creature/aluneth/vo_703_aluneth_58.ogg",	-- You're alone, child?
+	1396824,--"sound/creature/aluneth/vo_703_aluneth_63.ogg",	-- Let them walk.
+	1396827,--"sound/creature/aluneth/vo_703_aluneth_66.ogg",	-- Realm to realm...
+};
