2c003e88c8e325dfa8f271ecbfa34afccc766ad7
Thex <thex-pieddroit@hotmail.com>
2019-07-11 17:06:29 -0400

    Added N'Zoth module from TwoSouls

---
+++ KillThemAll_NZoth/Core.lua
@@ -0,0 +1,35 @@
+
+g_cerberus.RegisterAddonModule("KillThemAll");
+
+local l_eventsListener = CreateFrame("Frame");
+local l_events = {};
+
+function l_events:ADDON_LOADED(sAddonName)
+
+	if sAddonName ~= "KillThemAll_NZoth" then
+		return;
+	end
+
+	if g_godsListSettings ~= nil then
+		if S_ktaOptions ~= nil and S_ktaOptions.sGods ~= nil then
+
+			local currentGods = GetWords(string.upper(S_ktaOptions.sGods));
+			if TableContains(currentGods, g_nZoth.sDataName) then
+				SetGods(currentGods, true);
+			end
+		end
+
+		AddGodToSettingsList(g_nZoth);
+	else
+		KTA_Print("An update is available for KillThemAll. KillThemAll_NZoth will not work until both are updated.");
+		return;
+	end
+end
+
+l_eventsListener:SetScript("OnEvent", function(self, sEvent, ...)
+	l_events[sEvent](self, ...);
+end);
+
+for k, v in pairs(l_events) do
+	l_eventsListener:RegisterEvent(k);
+end
