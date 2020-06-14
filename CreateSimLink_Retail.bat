d778cf3eb51d6e60d11f05024d70aeac846f8c18
Thex <thex-pieddroit@hotmail.com>
2020-06-14 11:36:24 -0400

    Enhanced CreateSimLink bat files + created DeleteSimLink bat files

--- CreateSimLink_Retail.bat
+++ CreateSimLink_Retail.bat
@@ -3,13 +3,28 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-set /p addon=Which addon would you like to link? 
 set "gamePath=C:\Games\World of Warcraft\"
 
 IF NOT EXIST "%gamePath%" set "gamePath=D:\Games\World of Warcraft\"
+IF NOT EXIST "%gamePath%" (
+	echo Game directory not found.
+	pause
+	exit
+)
 
 :MakeLink
-mklink /D "%gamePath%_retail_\Interface\AddOns\%addon%" "%CD%\%addon%"
+set /p addon=Which addon would you like to link? 
+set "addonPath=%CD%\%addon%"
 
-set /p addon=Anything else? 
+IF NOT EXIST "%addonPath%" (
+	echo The addon doesn't exist.
+	goto MakeLink
+)
+
+set "linkPath=%gamePath%_retail_\Interface\AddOns\%addon%"
+
+IF NOT EXIST "%linkPath%" (
+	mklink /D "%linkPath%" "%addonPath%"
+) ELSE echo A link for this addon already exists.
+
 goto MakeLink
--- DeleteSimLink_Retail.bat
+++ DeleteSimLink_Retail.bat
@@ -4,6 +4,7 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 set "gamePath=C:\Games\World of Warcraft\"
+
 IF NOT EXIST "%gamePath%" set "gamePath=D:\Games\World of Warcraft\"
 IF NOT EXIST "%gamePath%" (
 	echo Game directory not found.
@@ -11,19 +12,13 @@ IF NOT EXIST "%gamePath%" (
 	exit
 )
 
-:MakeLink
-set /p addon=Which addon would you like to link? 
-set "addonPath=%CD%\%addon%"
+:DeleteLink
+set /p addon=Which addon's link would you like to delete? 
+set "addonPath=%gamePath%_retail_\Interface\AddOns\%addon%"
 
-IF NOT EXIST "%addonPath%" (
-	echo The addon doesn't exist.
-	goto MakeLink
-)
+IF EXIST "%addonPath%" (
+	rmdir /Q "%addonPath%"
+	echo Successfully deleted.
+) ELSE echo The link to this addon does not exist.
 
-set "linkPath=%gamePath%_classic_\Interface\AddOns\%addon%"
-
-IF NOT EXIST "%linkPath%" (
-	mklink /D "%linkPath%" "%addonPath%"
-) ELSE echo A link for this addon already exists.
-
-goto MakeLink
+goto DeleteLink
